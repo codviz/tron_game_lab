f7c969027e53a0bef9d43a902747f722c0d8057a
codviz <89945886+codviz@users.noreply.github.com>
2024-08-01 11:35:59 -0400

    Update tron_game.py

--- tron_game.py
+++ tron_game.py
@@ -1,7 +1,10 @@
 import grid_graphics as gg
 from human_player import HumanPlayer
+from enemy_player import EnemyPlayer
+
 
 human_player = HumanPlayer(5,20,0)
+enemy_player = EnemyPlayer(55,20,2)
 points_occupied = []
 
 #game states dictionary
@@ -24,11 +27,45 @@ def game_loop():
 def update():
     #method called every loop cycle
     gg.paint((human_player.x_pos,human_player.y_pos),'blue')
+    gg.paint((enemy_player.x_pos,enemy_player.y_pos),'red')
 
+    for i in range (61):
+        gg.paint((i,0),'white')
+        gg.paint((i,40),'white')
+
+    for i in range(1,40):
+        gg.paint((0,i),'white')
+        gg.paint((60,i),'white')
+
+    points_occupied.append((human_player.x_pos,human_player.y_pos))
+    points_occupied.append((enemy_player.x_pos,enemy_player.y_pos))
+    human_player.update_position()
+    enemy_player.update_position(points_occupied)
+
+    if (human_player.x_pos,human_player.y_pos) in points_occupied:
+        g['game_on'] = False
+        print('win')
+    if (enemy_player.x_pos,enemy_player.y_pos) in points_occupied:
+        g['game_on'] = False
+        print('lose')
+    if (enemy_player.x_pos,enemy_player.y_pos) == (human_player.x_pos,human_player.y_pos):
+        g['game_on'] = False
+        print('tie')
 
 def key_handler(k):
     #already enabled, k equals the string value of the key pressed
-    pass
+    if k == 'a':
+        human_player.change_direction('left')
+    if k == 'd':
+        human_player.change_direction('right')
+
+for i in range (61):
+    points_occupied.append((i,0))
+    points_occupied.append((i,40))
+
+for i in range(1,40):
+    points_occupied.append((0,i))
+    points_occupied.append((61,i))
 
 if __name__ == "__main__":
     play()
